76990f0c7672cc521c56686858c7d30ee397b065
Aleksbgbg <aleks.todorov.1337@gmail.com>
2023-02-06 22:02:39 +0000

    Add basic ppm image file.

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Ray Tracing
 
 [Ray Tracing in One Weekend](https://raytracing.github.io/books/RayTracingInOneWeekend.html) in Rust.
+
+## Dev Workflow
+
+### Run
+
+`cargo run > image.ppm`
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
 fn main() {
-  println!("Hello, world!");
+  const IMAGE_WIDTH: usize = 256;
+  const IMAGE_HEIGHT: usize = 256;
+
+  const LAST_PIXEL_X: usize = IMAGE_WIDTH - 1;
+  const LAST_PIXEL_Y: usize = IMAGE_HEIGHT - 1;
+
+  println!("P3");
+  println!("{IMAGE_WIDTH} {IMAGE_HEIGHT}");
+  println!("255");
+
+  for row in (0..IMAGE_HEIGHT).rev() {
+    for col in 0..IMAGE_WIDTH {
+      let red = col as f64 / LAST_PIXEL_X as f64;
+      let green = row as f64 / LAST_PIXEL_Y as f64;
+      let blue = 0.25;
+
+      let red_scaled = 255f64 * red;
+      let green_scaled = 255f64 * green;
+      let blue_scaled = 255f64 * blue;
+
+      println!("{red_scaled} {green_scaled} {blue_scaled}");
+    }
+  }
 }
